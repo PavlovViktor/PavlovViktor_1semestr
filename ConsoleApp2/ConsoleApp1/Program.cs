3803f6f57310a53f27db4bf001d9d0245ff7133e
PavlovViktor <vikpav070907@gmail.com>
2024-10-11 11:19:13 +0300

    Add files via upload
    8 практика на половину выполненая

---
+++ ConsoleApp2/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+          if (num1 >5)
+            {
+                Console.WriteLine
+            }
+            {
+                Console.WriteLine("Первое число больше второго");
+            }
+
+            {
+                Console.WriteLine("Первое число меньше второго");
+            }
+            else
+            {
+                Console.WriteLine("Оба числа равны");
+            }
+            Console.ReadKey();
+        }
+    }
+}
