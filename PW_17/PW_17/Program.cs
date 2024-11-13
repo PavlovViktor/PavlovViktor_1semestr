bcb892b63262e6d7cb6a17834b7881380f6464cd
PavlovViktor <vikpav070907@gmail.com>
2024-11-13 16:16:04 +0300

    Практика 17

---
+++ PW_17/PW_17/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+
+        string[] colors = { "red", "green", "black", "white", "blue" };
+
+
+        string filePath = @"C:\example\colors.txt";
+
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (string color in colors)
+                {
+                    writer.WriteLine(color);
+                }
+            }
+
+            Console.WriteLine("Элементы массива успешно записаны в файл.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка при записи в файл: {ex.Message}");
+        }
+    }
+}
