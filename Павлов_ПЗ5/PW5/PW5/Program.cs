a0ae9c3c552baf81dbc437ce322c09f5f7651257
PavlovViktor <vikpav070907@gmail.com>
2024-11-18 08:44:58 +0300

    практика 5

---
+++ Павлов_ПЗ5/PW5/PW5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double x = 4;
+        double answer = SolveEquation(x);
+        Console.WriteLine($"Решение уравнения 5*x^2 - 12*x + 4.6/-15*x при x={x} равно {answer}.");
+    }
+
+    private static double SolveEquation(double x)
+    {
+        double firstTerm = 5 * Math.Pow(x, 2);
+        double secondTerm = -12 * x;
+        double thirdTerm = 4.6 / (-15 * x);
+        return firstTerm + secondTerm + thirdTerm;
+    }
+}
