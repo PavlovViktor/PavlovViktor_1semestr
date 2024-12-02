3eda0b80b0317509bcd574f52d1dbcb62163b709
PavlovViktor <vikpav070907@gmail.com>
2024-12-02 11:20:10 +0300

    Add files via upload

---
+++ практика 21/задание 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Circle
+{
+
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Radius { get; set; }
+
+
+    public Circle()
+    {
+        X = 0;
+        Y = 0;
+        Radius = 0;
+    }
+
+    public Circle(int radius)
+    {
+        X = 0;
+        Y = 0;
+        Radius = radius;
+    }
+
+
+    public Circle(int x, int y)
+    {
+        X = x;
+        Y = y;
+        Radius = 0;
+    }
+
+   
+    public Circle(int x, int y, int radius)
+    {
+        X = x;
+        Y = y;
+        Radius = radius;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Центр круга: ({X}, {Y}), Радиус: {Radius}");
+    }
+}
---
+++ практика 21/практика 21/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+public class AverageMethods
+{
+   
+    public static double Average(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+            throw new ArgumentException("Массив не может быть пустым или равен null", nameof(numbers));
+
+        int sum = 0;
+        foreach (var number in numbers)
+        {
+            sum += number;
+        }
+
+        return (double)sum / numbers.Length;
+    }
+
+   
+    public static double Average(float[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+            throw new ArgumentException("Массив не может быть пустым или равен null", nameof(numbers));
+
+        float sum = 0; // Автоматически преобразуется в float
+        foreach (var number in numbers)
+        {
+            sum += number;
+        }
+
+        return (double)sum / numbers.Length;
+    }
+}
